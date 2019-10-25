92cced1a42974458e77dc9b24cfcbab589431102
LissetMtz <56983813+LissetMtz@users.noreply.github.com>
2019-10-25 17:41:49 -0500

    Add files via upload

---
+++ LEARN PYTHON 3 THE HARD WAY REVIEW.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # LEARN PYTHON 3 THE HARD WAY
+
+# ## *REVIEW*
+
+#  #### INTRODUCTION
+
+# The book explains how to start programming quickly with many examples, starting in how to install, to open
+# and run the program.
+# It has the advantage to explain in two differents operative systems what you have (macOS and Windows), furthermore you can train with Study Drills and finally you can check the common that the students ask about the program in a special section called Common Student Question.
+
+# ### A Good First Program
+
+# The first exercise that the book has is about how to create a first good program, this is important to be able to start with everything related to python programming, so the first program that the writer shows is the famous "Hello World".
+
+# In[1]:
+
+
+print ("¡Hello World!")
+
+
+# ### Comments and Pound Characters
+
+# Comments are something very important for any programmer, because it is a very easy way to understand and that other people understand what was done in that line of the code, this also helps if you have a line in your code which does not work or you are not sure if it should be there, and you don't want to delete it so you add it as a comment in case in the future it can help you.
+
+# In[2]:
+
+
+print("2+5<3+5 15 is true or false?") #This is a comment
+print (2+5<3+5)
+
+
+# ### Numbers and Math
+
+# In this third exercise, the writer talks about the different symbols that exist in this programming language, which is a very important tool to be able to perform both basic and even more complex operations.
+
+# In[3]:
+
+
+print (2 + 2) # Sum
+print (2 - 3) #Rest
+print (8 / 4) #Divison
+print ( 4 * 2) #Multiplication
+
+
+# In[ ]:
+
+
+
+
